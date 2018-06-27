d5cf644860c5e1293a940554bf74a8ca5ab6b72a
Warren Leyes <2774431+WLeyes@users.noreply.github.com>
2018-06-27 12:19:35 -0700

    created local env added node deps and started to flesh out html

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
-console.log('It works.');
+new Vue ({
+  el: '#app',
+  data: {
+    total: 0,
+    items: [
+      { title: 'Item 1' },
+      { title: 'Item 2' },
+      { title: 'Item 3' },
+      { title: 'Item 4' },
+    ]
+  },
+  methods: {
+    addItem: function() {
+      this.total += 9.99;
+    }
+  }
+});
