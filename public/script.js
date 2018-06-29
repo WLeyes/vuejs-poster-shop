7a5090a8372924297c81704530eabe6e8e1a1e39
Warren Leyes <2774431+WLeyes@users.noreply.github.com>
2018-06-29 11:34:28 -0700

    added addItem method

--- public/script.js
+++ public/script.js
@@ -3,17 +3,37 @@ new Vue ({
   data: {
     total: 0,
     items: [
-      { title: 'Item 1' },
-      { title: 'Item 2' },
-      { title: 'Item 3' },
-      { title: 'Item 4' },
+      { id: 1, title: 'Item 1', price: 9.99 },
+      { id: 2, title: 'Item 2', price: 19.99  },
+      { id: 3, title: 'Item 3', price: 9.99  },
+      { id: 4, title: 'Item 4', price: 49.97  },
     ],
     cart: []
   },
   methods: {
     addItem: function(index) {
-      this.total += 9.99;
-      this.cart.push(this.items[index]);
+      this.total += this.items[index].price;
+      let item = this.items[index];
+      let found = false;
+      for(let i = 0; i < this.cart.length; i++){
+        if(this.cart[i].id === item.id){
+          found = true;
+          this.cart[i].qty++;
+        }
+      }
+      if(!found) {
+        this.cart.push({
+          id: item.id,
+          title: item.title,
+          price: item.price,
+          qty: 1
+        });
+      }
+    }
+  },
+  filters: {
+    currency: function(price) {
+      return '$'.concat(price.toFixed(2));
     }
   }
 });
